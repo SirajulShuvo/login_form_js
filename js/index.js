ce97d3656c17fe8ba72cc461f62fa86457f32573
SirajMia <sirajulshuvo08@gmail.com>
2024-03-01 16:50:53 +0600

    one

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
-// item select
+function loginFormJs(){
+    // item select
 let nameField = document.getElementById('nameField');
 let title  = document.getElementById('title');
 let underline = document.getElementById('underline');
 let signUp = document.getElementById('mySignUp');
 let signIn = document.getElementById('mySignIn');
+let p1 = document.getElementById('p1');
 
 signIn.addEventListener('click',(e) => {
     nameField.style.maxHeight = '0';
@@ -11,6 +13,7 @@ signIn.addEventListener('click',(e) => {
     signUp.classList.add('disabled')
     signIn.classList.remove('disabled')
     underline.style.transform = 'translateX(35px)'
+    p1.innerHTML = 'Lost password';
 })
 
 signUp.addEventListener('click',(e) =>{
@@ -19,6 +22,7 @@ signUp.addEventListener('click',(e) =>{
     signIn.classList.add('disabled')
     signUp.classList.remove('disabled')
     underline.style.transform = 'translateX(-25px)'
+    p1.innerHTML = 'Forget password';
 });
-
-console.log('helo')
+}
+loginFormJs();
